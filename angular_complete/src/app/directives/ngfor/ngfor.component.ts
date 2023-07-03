a8a2aaeb39e14e6a32f6c1fdbe26453d9445e113
shivsoni <sonishiv309@gmail.com>
2023-07-03 17:59:17 +0530

    added *ngFor directives.

--- angular_complete/src/app/directives/ngfor/ngfor.component.ts
+++ angular_complete/src/app/directives/ngfor/ngfor.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./ngfor.component.css']
 })
 export class NgforComponent {
-
+person:any=[
+  {name:'shivsoni',age:22,address:'raipur'},
+  {
+    name:'aliyasoni',age:5,address:'durg'
+  },{
+    name:'riyasoni',age:3,address:'raipur'
+  },
+  {
+    name:'pushpasoni',age:33,address:'raipur'
+  },{
+    name:'payalsoni',age:20,address:'raipur'
+  }
+]
 }
