59b366aa8e1c6179b45da96f64c1d0ebadef2caa
shivsoni <sonishiv309@gmail.com>
2023-07-03 12:28:02 +0530

    added manually created module.

--- angular_complete/src/app/app.module.ts
+++ angular_complete/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2shivComponent } from './comp2/comp2.component';
+import { ManualModule } from './manual_module/manual_module.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { Comp2shivComponent } from './comp2/comp2.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ManualModule,CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
