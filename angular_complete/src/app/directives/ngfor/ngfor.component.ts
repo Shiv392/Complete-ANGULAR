d2d19892410acca088f721d0006062f085fab8e7
shivsoni <sonishiv309@gmail.com>
2023-07-04 10:35:05 +0530

    added pagination and sorting in *ngFor table

--- angular_complete/src/app/directives/ngfor/ngfor.component.ts
+++ angular_complete/src/app/directives/ngfor/ngfor.component.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./ngfor.component.css']
 })
 export class NgforComponent {
-person:any=[
+  currentPage: number = 1;
+  itemsPerPage: number = 2;
+  totalItems: number = 0;
+  sortedColumn: string = '';
+  sortOrder: string = 'asc';
+
+person:any[]=[
   {name:'shivsoni',age:22,address:'raipur'},
   {
     name:'aliyasoni',age:5,address:'durg'
@@ -19,4 +25,53 @@ person:any=[
     name:'payalsoni',age:20,address:'raipur'
   }
 ]
+
+  // Sorting function
+  sortData(column: string) {
+    if (column === this.sortedColumn) {
+      this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortedColumn = column;
+      this.sortOrder = 'asc';
+    }
+
+    // Perform sorting logic on your person array here
+    this.person.sort((a, b) => {
+      if (this.sortOrder === 'asc') {
+        return a[column] > b[column] ? 1 : -1;
+      } else {
+        return a[column] < b[column] ? 1 : -1;
+      }
+    });
+
+    // Update pagination after sorting
+    this.currentPage = 1;
+  }
+
+  // Pagination functions
+  goToPage(page: number) {
+    this.currentPage = page;
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  get totalPages() {
+    return Math.ceil(this.person.length / this.itemsPerPage);
+  }
+
+  get visibleItems() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.person.slice(startIndex, endIndex);
+  }
 }
